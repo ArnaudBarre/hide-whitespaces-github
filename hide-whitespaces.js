96cf14bd4a8a6d94248717de32edb1ab2ae61bf4
Arnaud Barré <arnaud@nabla.com>
2020-12-14 12:22:32 +0100

    - Fix notification links that already contains search params - Use replace + reload to avoid polluting history (and allow back)

--- hide-whitespaces.js
+++ hide-whitespaces.js
@@ -7,13 +7,16 @@ window.onclick = function () {
 };
 
 function updateUrlIfNeeded() {
-  if (
-    currentURL.match(/github.com\/.*\/.*\/pull\/.*\/files/) &&
-    !location.search
-  ) {
-    console.log("Hiding whitespaces change");
-    location.search = "?diff=split&w=1";
-  }
+  if (!currentURL.match(/github.com\/.*\/.*\/pull\/.*\/files/)) return;
+  const search = new URLSearchParams(location.search);
+  if (search.get("w")) return;
+  search.set("w", "1");
+  window.history.replaceState(
+    null,
+    null,
+    `${location.href}?${search.toString()}`,
+  );
+  location.reload();
 }
 
 updateUrlIfNeeded();
