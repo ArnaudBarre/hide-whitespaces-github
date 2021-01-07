8fabca7ace50f67bcc21b0411a9861782813823a
Arnaud Barré <arnaud@nabla.com>
2021-01-07 20:56:58 +0100

    Fix bad handling of search params with hash

--- hide-whitespaces.js
+++ hide-whitespaces.js
@@ -8,14 +8,10 @@ window.onclick = function () {
 
 function updateUrlIfNeeded() {
   if (!currentURL.match(/github.com\/.*\/.*\/pull\/.*\/files/)) return;
-  const search = new URLSearchParams(location.search);
-  if (search.get("w")) return;
-  search.set("w", "1");
-  window.history.replaceState(
-    null,
-    null,
-    `${location.href}?${search.toString()}`,
-  );
+  const url = new URL(location);
+  if (url.searchParams.get("w")) return;
+  url.searchParams.set("w", "1");
+  window.history.replaceState(null, null, url.toString());
   location.reload();
 }
 
